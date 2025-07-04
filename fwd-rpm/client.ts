15ac790210f383d9e657624caf2939e21b1a1b3b
ssande-fwd <ssande@forwardedu.com>
2025-07-04 15:15:03 -0400

    rpm client

---
+++ fwd-rpm/client.ts
@@ -0,0 +1,28 @@
+namespace fwdSensors {
+    //% fixedInstances
+    export class FwdRpmClient extends jacdac.SimpleSensorClient {
+        constructor(role: string) {
+            super(0x19f8e291, role, "i24.8")
+        }
+
+        /**
+         * Speed measured by the sensor.
+         * Positive is recommended to be clockwise rotation and negative counterclockwise
+         */
+        //% callInDebugger
+        //% group="Speed"
+        //% weight=1
+        speed(): number {
+            return this.reading()
+        }
+    }
+
+    //% fixedInstance whenUsed
+    export const rpm1 = new FwdRpmClient("RPM1")
+    //% fixedInstance whenUsed
+    export const rpm2 = new FwdRpmClient("RPM2")
+    //% fixedInstance whenUsed
+    export const rpm3 = new FwdRpmClient("RPM3")
+    //% fixedInstance whenUsed
+    export const rpm4 = new FwdRpmClient("RPM4")
+}
