5d7fa10986204bc414400269a88d5f0895f39fdb
ssande-fwd <ssande@forwardedu.com>
2025-07-31 12:55:52 -0400

    ec client code

--- fwd-ec/client.ts
+++ fwd-ec/client.ts
@@ -2,162 +2,48 @@ namespace modules {
     /**
      * A sensor measuring ElectricalConductivity.
      **/
-    //% fixedInstances blockGap=8
-    export class ElectricalConductivityClient extends jacdac.SimpleSensorClient {
-        private readonly _electricalConductivityError: jacdac.RegisterClient<
-            [number]
-        >
-        private readonly _minElectricalConductivity: jacdac.RegisterClient<
-            [number]
-        >
-        private readonly _maxElectricalConductivity: jacdac.RegisterClient<
-            [number]
-        >
-
+    //% fixedInstances
+    export class FwdEcClient extends modules.ElectricalConductivityClient {
         constructor(role: string) {
-            super(
-                jacdac.SRV_ELECTRICALCONDUCTIVITY,
-                role,
-                jacdac.ElectricalConductivityRegPack.ElectricalConductivity
-            )
-
-            this._electricalConductivityError = this.addRegister<[number]>(
-                jacdac.ElectricalConductivityReg.ElectricalConductivityError,
-                jacdac.ElectricalConductivityRegPack
-                    .ElectricalConductivityError,
-                jacdac.RegisterClientFlags.Optional
-            )
-            this._minElectricalConductivity = this.addRegister<[number]>(
-                jacdac.ElectricalConductivityReg.MinElectricalConductivity,
-                jacdac.ElectricalConductivityRegPack.MinElectricalConductivity,
-                jacdac.RegisterClientFlags.Optional |
-                    jacdac.RegisterClientFlags.Const
-            )
-            this._maxElectricalConductivity = this.addRegister<[number]>(
-                jacdac.ElectricalConductivityReg.MaxElectricalConductivity,
-                jacdac.ElectricalConductivityRegPack.MaxElectricalConductivity,
-                jacdac.RegisterClientFlags.Optional |
-                    jacdac.RegisterClientFlags.Const
-            )
+            super(role)
         }
 
         /**
-         * The ElectricalConductivity, uS, of fluids.
+         * Returns the sensor's relative EC reading (mV).
          */
-        //% callInDebugger
-        //% group="Environment"
-        //% block="%electricalconductivity electricalconductivity (uS)"
-        //% blockId=jacdac_electricalconductivity_electricalconductivity___get
-        //% weight=100
-        electricalconductivity(): number {
-            return this.reading()
+        //% group="Relative EC"
+        //% block="$this mV"
+        //% blockId=fwd_ec_get_ec
+        ec(): number {
+            return super.reading()
         }
 
         /**
-         * Error on the electricalconductivity reading.
+         * Returns true when the relative EC (mV) is past the provided threshold in the designated direction.
+         * @param threshold the relative EC (mV) to check against
+         * @param direction over or under the threshold
          */
-        //% callInDebugger
-        //% group="Environment"
-        //% weight=99
-        electricalconductivityError(): number {
-            this.start()
-            const values =
-                this._electricalConductivityError.pauseUntilValues() as any[]
-            return values[0]
-        }
-
-        /**
-         * Lowest electricalconductivity that can be reported.
-         */
-        //% callInDebugger
-        //% group="Environment"
-        //% weight=98
-        minElectricalConductivity(): number {
-            this.start()
-            const values =
-                this._minElectricalConductivity.pauseUntilValues() as any[]
-            return values[0]
-        }
-
-        /**
-         * Highest electricalconductivity that can be reported.
-         */
-        //% callInDebugger
-        //% group="Environment"
-        //% weight=97
-        maxElectricalConductivity(): number {
-            this.start()
-            const values =
-                this._maxElectricalConductivity.pauseUntilValues() as any[]
-            return values[0]
-        }
-
-        /**
-         * Run code when the electricalconductivity changes by the given threshold value.
-         */
-        //% group="Environment"
-        //% blockId=jacdac_electricalconductivity_on_electricalconductivity_change
-        //% block="on %electricalconductivity electricalconductivity changed by %threshold (uS)"
-        //% weight=96
-        //% threshold.min=0
-        //% threshold.max=10.5
-        //% threshold.defl=1
-        onElectricalConductivityChangedBy(
+        //% group="Relative EC"
+        //% block="$this is $direction $threshold mV"
+        //% blockId=fwd_ec_is_past_threshold
+        isPastThreshold(
             threshold: number,
-            handler: () => void
-        ): void {
-            this.onReadingChangedBy(threshold, handler)
-        }
-
-        /**
-         * Sets the low reference calibration point.
-         * @param low low uS value
-         */
-        //% blockId=jacdac_characterscreen_setcalibrationlow_cmd
-        //% block="set the low reference calibration on %electricalconductivity|to low %low"
-        //% low.defl=1413
-        //% weight=88
-        //% group="Environment"
-        setlowreference(low: number): void {
-            this.start()
-            this.sendCommand(
-                jacdac.JDPacket.from(
-                    jacdac.ElectricalConductivityCmd.CalibrateLow,
-                    jacdac.jdpack(
-                        jacdac.ElectricalConductivityCmdPack
-                            .SetCalibrationPoint,
-                        [low]
-                    )
-                )
-            )
-        }
-
-        /**
-         * Sets the high reference calibration point.
-         * @param high high uS value
-         */
-        //% blockId=jacdac_characterscreen_setcalibrationhigh_cmd
-        //% block="set the high reference calibration on %electricalconductivity|to high %high"
-        //% high.defl=5000
-        //% weight=88
-        //% group="Environment"
-        sethighreference(high: number): void {
-            this.start()
-            this.sendCommand(
-                jacdac.JDPacket.from(
-                    jacdac.ElectricalConductivityCmd.CalibrateHigh,
-                    jacdac.jdpack(
-                        jacdac.ElectricalConductivityCmdPack
-                            .SetCalibrationPoint,
-                        [high]
-                    )
-                )
-            )
+            direction: fwdEnums.OverUnder
+        ): boolean {
+            const difference = this.ec() - threshold > 0
+            const isPastThreshold =
+                (direction === fwdEnums.OverUnder.Over && difference) ||
+                (direction === fwdEnums.OverUnder.Under && !difference)
+            return isPastThreshold
         }
     }
 
-    //% fixedInstance whenUsed weight=1 block="electricalconductivity1"
-    export const electricalconductivity1 = new ElectricalConductivityClient(
-        "electricalconductivity1"
-    )
+    //% fixedInstance whenUsed
+    export const ec1 = new FwdEcClient("ec1")
+    //% fixedInstance whenUsed
+    export const ec2 = new FwdEcClient("ec2")
+    //% fixedInstance whenUsed
+    export const ec3 = new FwdEcClient("ec3")
+    //% fixedInstance whenUsed
+    export const ec4 = new FwdEcClient("ec4")
 }
