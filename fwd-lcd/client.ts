fe84d5e8226758a046fa826896da98b4fa2cfb34
ssande-fwd <ssande@forwardedu.com>
2025-09-11 15:01:15 +0000

    test 40 ms delay

--- fwd-lcd/client.ts
+++ fwd-lcd/client.ts
@@ -37,7 +37,7 @@ namespace fwdSensors {
 
     //% fixedInstances blockGap=8
     export class LCDClient extends modules.CursorCharacterScreenClient {
-        private readonly delay = 30
+        private readonly delay = 40
 
         constructor(role: string) {
             super(role)
