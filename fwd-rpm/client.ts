5c62e7586b882992cfe0289f420b0085a0a1dea8
ssande-fwd <ssande@forwardedu.com>
2025-09-11 18:40:35 +0000

    Update fwd-rpm/pxt.json, fwd-rpm/client.ts, fwd-rpm/test.ts

--- fwd-rpm/client.ts
+++ fwd-rpm/client.ts
@@ -1,20 +1,40 @@
 namespace fwdSensors {
     //% fixedInstances
-    export class FwdRpmClient extends jacdac.SimpleSensorClient {
+    export class FwdRpmClient extends modules.RotationsPerMinuteClient {
         constructor(role: string) {
-            super(0x19f8e291, role, "i24.8")
+            super(role)
         }
 
         /**
          * Speed measured by the sensor.
-         * Positive is recommended to be clockwise rotation and negative counterclockwise
+         * Positive values are clockwise rotation and negative are counterclockwise.
          */
         //% group="RPM"
         //% block="$this RPM"
         //% blockId=fwd_rpm_get_rpm
         //% weight=100
         rpm(): number {
-            return this.reading()
+            return Math.round(super.reading())
+        }
+
+        /**
+         * Returns true when the RPM is past the provided threshold in the designated direction.
+         * @param threshold the RPM value to check against
+         * @param direction over or under the threshold
+         */
+        //% group="RPM"
+        //% block="$this is $direction $threshold RPM"
+        //% blockId=fwd_rpm_is_past_threshold
+        //% weight=99
+        isPastThreshold(
+            threshold: number,
+            direction: fwdEnums.OverUnder
+        ): boolean {
+            const difference = this.rpm() - threshold > 0
+            const isPastThreshold =
+                (direction === fwdEnums.OverUnder.Over && difference) ||
+                (direction === fwdEnums.OverUnder.Under && !difference)
+            return isPastThreshold
         }
     }
 
--- fwd-rpm/test.ts
+++ fwd-rpm/test.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-    console.log(fwdSensors.rpm1.rpm())
-})
+// RPM Sensor Tests
+// Type: compilation
+//  rpm(): number
+//  isPastThreshold(threshold: number, direction: fwdEnums.OverUnder): boolean
+if (fwdSensors.rpm1.isPastThreshold(fwdSensors.rpm1.rpm(), fwdEnums.OverUnder.Over)) {
+
+}
