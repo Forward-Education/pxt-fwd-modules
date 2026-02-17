8cda6a11b65b83e3cbb73a8ee6e52dcbbf05180a
ssande-fwd <ssande@forwardedu.com>
2026-02-17 17:58:06 -0500

    update deps

--- fwd-servo-continuous/blocks.ts
+++ fwd-servo-continuous/blocks.ts
@@ -1,22 +1,5 @@
 namespace fwdMotors {
     /**
-     * Set the servo to enabled or disabled.
-     * @param servo the servo client to set the status of
-     * @param state enabled = true, disabled = false
-     */
-    //% group="Servo - Continuous"
-    //% block="turn $servo $state"
-    //% blockId=fwd_servocon_set_enabled
-    //% state.shadow="toggleOnOff"
-    //% weight=100
-    export function conSetEnabled(
-        servo: fwdBase.FwdServoClient,
-        state: boolean
-    ): void {
-        return servo.setEnabled(state)
-    }
-
-    /**
      * Set the servo speed to between 100% and -100%. Negative speeds are the reverse direction.
      * @param servo the servo client to set the speed of
      * @param speed the speed to set the servo to (%)
@@ -28,7 +11,7 @@ namespace fwdMotors {
     //% weight=99
     export function setSpeed(
         servo: fwdBase.FwdServoClient,
-        speed: number
+        speed: number,
     ): void {
         servo.setSpeed(speed)
     }
@@ -43,17 +26,5 @@ namespace fwdMotors {
     //% weight=98
     export function getSpeed(servo: fwdBase.FwdServoClient): number {
         return servo.getSpeed()
-    }
-
-    /**
-     * Returns whether the servo is enabled, enabled = true, disabled = false.
-     * @param servo the servo client to get the status of
-     */
-    //% group="Servo - Continuous"
-    //% block="$servo is enabled"
-    //% blockId=fwd_servocon_is_enabled
-    //% weight=97
-    export function conIsEnabled(servo: fwdBase.FwdServoClient): boolean {
-        return servo.enabled()
     }
 }
--- fwd-servo-positional/blocks.ts
+++ fwd-servo-positional/blocks.ts
@@ -1,22 +1,5 @@
 namespace fwdMotors {
     /**
-     * Set the servo to enabled or disabled.
-     * @param servo the servo client to set the status of
-     * @param state enabled = true, disabled = false
-     */
-    //% group="Servo - 270° Positional"
-    //% block="turn $servo $state"
-    //% blockId=fwd_servopos_set_enabled
-    //% state.shadow="toggleOnOff"
-    //% weight=100
-    export function posSetEnabled(
-        servo: fwdBase.FwdServoClient,
-        state: boolean
-    ): void {
-        return servo.setEnabled(state)
-    }
-
-    /**
      * Set the angle of the servo and immediately run the next block.
      * @param servo the servo client to set the angle of
      * @param angle servo angle
@@ -28,7 +11,7 @@ namespace fwdMotors {
     //% weight=99
     export function setAngle(
         servo: fwdBase.FwdServoClient,
-        angle: number
+        angle: number,
     ): void {
         servo.setAngle(angle)
     }
@@ -45,7 +28,7 @@ namespace fwdMotors {
     //% weight=98
     export function setAngleAndWait(
         servo: fwdBase.FwdServoClient,
-        angle: number
+        angle: number,
     ): void {
         servo.setAngleAndWait(angle)
     }
@@ -60,52 +43,5 @@ namespace fwdMotors {
     //% weight=97
     export function getAngle(servo: fwdBase.FwdServoClient): number {
         return servo.getAngle()
-    }
-
-    /**
-     * Preset servo positions based on a clock's hour hand.
-     * @param position pos0 through pos9 where the number represents the clock hour position
-     */
-    //% group="Servo - 270° Positional"
-    //% block="position $position"
-    //% blockId=fwd_servopos_position_presets
-    //% weight=96
-    export function positionPresets(position: ServoClockPositions): number {
-        return position as number
-    }
-
-    /**
-     * Returns whether the servo is enabled, enabled = true, disabled = false.
-     * @param servo the servo client to get the status of
-     */
-    //% group="Servo - 270° Positional"
-    //% block="$servo is enabled"
-    //% blockId=fwd_servopos_is_enabled
-    //% weight=95
-    export function posIsEnabled(servo: fwdBase.FwdServoClient): boolean {
-        return servo.enabled()
-    }
-
-    export const enum ServoClockPositions {
-        //% block="🕛 00:00"
-        Position0 = 270,
-        //% block="🕐 01:00"
-        Position1 = 240,
-        //% block="🕑 02:00"
-        Position2 = 210,
-        //% block="🕒 03:00"
-        Position3 = 180,
-        //% block="🕓 04:00"
-        Position4 = 150,
-        //% block="🕔 05:00"
-        Position5 = 120,
-        //% block="🕕 06:00"
-        Position6 = 90,
-        //% block="🕖 07:00"
-        Position7 = 60,
-        //% block="🕗 08:00"
-        Position8 = 30,
-        //% block="🕘 09:00"
-        Position9 = 0,
     }
 }
