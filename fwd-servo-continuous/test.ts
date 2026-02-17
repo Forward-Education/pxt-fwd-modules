0ace9a7f83ab18f5237ee6e6dbdf16b75a6d134c
ssande-fwd <ssande@forwardedu.com>
2026-02-17 18:30:10 -0500

    fix test

--- fwd-servo-continuous/test.ts
+++ fwd-servo-continuous/test.ts
@@ -2,13 +2,9 @@
 // Type: compilation
 //  setSpeed(servo: fwdBase.FwdServoClient, speed: number): void
 //  getSpeed(servo: fwdBase.FwdServoClient): number
-//  setupDriving(left: fwdBase.FwdServoClient, right: fwdBase.FwdServoClient, bias = 0)
-//  drive(direction: fwdEnums.ForwardReverse, speed: number)
-//  stop()
-//  turn(angle: number)
-fwdMotors.setupDriving(fwdBase.leftServo, fwdBase.leftServo)
+//  setupDriving(left: fwdBase.FwdServoClient, right: fwdBase.FwdServoClient)
+//  function drive(leftSpeed: number, rightSpeed: number, duration: number)
 fwdMotors.setSpeed(fwdBase.leftServo, 0)
-fwdMotors.drive(fwdEnums.ForwardReverse.Forward, 50)
-fwdMotors.turn(0)
 basic.showNumber(fwdMotors.getSpeed(fwdBase.leftServo))
-fwdMotors.stop()
+fwdMotors.setupDriving(fwdBase.leftServo, fwdBase.leftServo)
+fwdMotors.drive(50, 50, 2)
